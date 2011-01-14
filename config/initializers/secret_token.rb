b14d30e71296c28306e74b09efe66340f30b214d
Todd Willey <xtoddx@gmail.com>
2011-01-13 20:45:30 -0500

    Initial checkin.  Blank rails 3 app with defaults.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Relish::Application.config.secret_token = '848003b2a76e7986d40450808f996b4ca7b1c08415682bfd02a04c29e26831cc59ba39e68f84dac899b0e9a070d1d7a44f4d20769a66a90801662bac7194b6ba'
