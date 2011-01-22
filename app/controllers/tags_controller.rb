5b2ed38f47ada9f01f7d6172313325599922b674
Chase Southard <chase.southard@gmail.com>
2011-01-22 18:48:04 -0500

    rescue_from empty bookmarks for a given tag.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,8 @@ class TagsController < ApplicationController
   before_filter :authenticate_user!, :except => [:show, :index]
   load_and_authorize_resource :find_by => :name
 
+  rescue_from ActiveRecord::RecordNotFound, :with => :no_bookmarks_for_tag
+
   def index
     respond_to do |format|
       format.html # index.html.erb
@@ -13,10 +15,14 @@ class TagsController < ApplicationController
     @bookmarks = @tag.taggings.collect { |tagging|   
       Bookmark.find(tagging.taggable_id)
     }
-
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @tag }
     end
+  end
+  
+  def no_bookmarks_for_tag
+    render "bookmarks/_no_bookmarks"
   end
 end
